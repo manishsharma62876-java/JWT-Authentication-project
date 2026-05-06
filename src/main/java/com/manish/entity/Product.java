48d508791b36e77e49d94743a49fe63f5560a3f5
ManishsharmaDEV <ms7054327@gmail.com>
2026-05-06 18:05:46 +0530

    initial update

--- src/main/java/com/manish/entity/Product.java
+++ src/main/java/com/manish/entity/Product.java
@@ -1,5 +1,17 @@
 package com.manish.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
 public class Product {
 
+	private Integer productId;
+	
+	private String productName;
 }
